ed1c6c5683e2552a94b9b6096bea42f3a09e2d14
UsikB <ni.github@gmail.com>
2022-10-17 12:52:06 +0300

    for review

--- src/test/java/praktikum/DatabaseTest.java
+++ src/test/java/praktikum/DatabaseTest.java
@@ -1,4 +1,24 @@
 package praktikum;
 
-public class DatabaseTest {
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class DatabaseTest extends TestCase {
+
+    Database database = new Database();
+    List<Bun> buns = database.availableBuns();
+    List<Ingredient> ingredients = database.availableIngredients();
+
+    @Test
+    public void testAvailableBuns() {
+        Assert.assertEquals(buns, database.availableBuns());
+    }
+
+    @Test
+    public void testAvailableIngredients() {
+        Assert.assertEquals(ingredients, database.availableIngredients());
+    }
 }
